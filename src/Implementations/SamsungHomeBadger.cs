a46052da545eaebf4d93560fa86c601a6dc1cc0c
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-12-22 22:18:03 +0300

    Added implementations

---
+++ src/Implementations/SamsungHomeBadger.cs
@@ -0,0 +1,21 @@
+using Android.Content;
+using Java.Lang;
+using ShortcutBadger.Infrastructure;
+
+namespace ShortcutBadger.Implementations
+{
+
+	/**
+	 * @author Leo Lin
+	 * Deprecated, Samesung devices will use DefaultBadger
+	 * https://github.com/leolin310148/ShortcutBadger/blob/master/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/SamsungHomeBadger.java
+	 */
+	[Deprecated]
+	internal class SamsungHomeBadger : BaseShortcutBadger
+	{
+		public SamsungHomeBadger(Context context)
+			: base(context)
+		{
+		}
+	}
+}
