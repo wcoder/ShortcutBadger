df06d81d05e9d7e0aabea989096742992b41d8b5
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-12-22 22:18:22 +0300

    Added example

---
+++ example/ExampleShortcutBadgerApp/ExampleShortcutBadgerApp/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ExampleShortcutBadgerApp
+{
+	[Activity(Label = "ExampleShortcutBadgerApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			SetContentView(Resource.Layout.Main);
+
+			var editText = FindViewById<EditText>(Resource.Id.badgeValue);
+			var button = FindViewById<Button>(Resource.Id.updateButton);
+
+			button.Click += delegate
+			{
+				var count = int.Parse(editText.Text);
+
+				ShortcutBadger.ShortcutBadger.SetBadge(this, count);
+			};
+		}
+	}
+}
+
