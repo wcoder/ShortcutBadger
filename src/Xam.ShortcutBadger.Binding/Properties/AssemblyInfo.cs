9045e3433f36b1279cff4b69f9df34f7878d31b1
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-06-19 23:24:43 +0300

    Add binding library

---
+++ src/Xam.ShortcutBadger.Binding/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Xam.ShortcutBadger.Binding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Yauheni Pakala")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.1.21")]
